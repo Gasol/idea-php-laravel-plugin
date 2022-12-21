4a644c2a758eeb45cd1cb48af2ef7f325961f04f
Gasol Wu <gasol.wu@gmail.com>
2022-12-21 09:43:20 +0800

    Fix build for 223

--- src/main/java/de/espend/idea/laravel/ReferenceResolver2.java
+++ src/main/java/de/espend/idea/laravel/ReferenceResolver2.java
@@ -1,21 +1,36 @@
 package de.espend.idea.laravel;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.php.lang.psi.PhpMultipleDeclarationFilter;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
-import com.jetbrains.php.lang.psi.elements.PhpReference;
-import com.jetbrains.php.lang.psi.resolve.PhpReferenceResolver2;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.Collections;
 
-@SuppressWarnings("UnstableApiUsage")
-public class ReferenceResolver2 implements PhpReferenceResolver2 {
+public class ReferenceResolver2 implements PhpMultipleDeclarationFilter {
+
+    private static final Logger logger = Logger.getInstance(ReferenceResolver2.class);
+
     @Override
-    public Collection<? extends PhpNamedElement> resolve(PhpReference phpReference, Collection<? extends PhpNamedElement> candidates) {
-        if (candidates.size()>1) {
-            for (PhpNamedElement element : candidates) {
-                if (element.getContainingFile().getVirtualFile().getName().equals("_ide_helper.php"))
-                    return Collections.singleton(element);
-            }
+    public <E extends PhpNamedElement> Collection<E> filter(@NotNull PsiElement psiElement, Collection<E> candidates) {
+        if (candidates.size() > 1) {
+            return ContainerUtil.filter(candidates, (PhpNamedElement candidate) -> {
+                final PsiFile file = candidate.getContainingFile();
+                if (file == null) {
+                    return false;
+                }
+                VirtualFile virtualFile = file.getVirtualFile();
+                if (virtualFile == null) {
+                    return false;
+                }
+
+                String name = virtualFile.getName();
+                return virtualFile.isValid() && name.startsWith("_ide_helper") && name.endsWith(".php");
+            });
         }
         return candidates;
     }
